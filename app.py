b0b9ae6a5639ab4200c68eb547aa840c370f99c8
Berkan Cetin <m.berkancetin@icloud.com>
2024-05-28 15:37:05 +0200

    Frontend development checkpoint-5

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ AI Powered Startup Success Prediction Tool.
 # StartOrb: Unlocking Tomorrow’s Success Today
 # “Predicting Success with StartOrb”
 # Subheadline: “Harness the Power of Data to Illuminate Your Startup Journey”
-st.image('images/orb.gif', caption='Orb gif image')
+# st.image('images/orb.gif', caption='Orb gif image')
 
 col1, col2 = st.columns(2)
 
