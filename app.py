5d00b8bc858d6d95f9733c9241acff51e15cc640
Berkan Cetin <m.berkancetin@icloud.com>
2024-05-28 14:09:52 +0200

    Frontend development checkpoint-3

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.markdown('''
 Please complete the form to see the startup success prediction.
 ''')
 
-with st.form("User input form", clear_on_submit=False, border=True):
+with st.form("User input form", clear_on_submit=True, border=True):
     founded_date = st.date_input('The company founded year',
                                     datetime.date(2019, 7, 6))
 
@@ -81,12 +81,35 @@ with st.form("User input form", clear_on_submit=False, border=True):
                             'Music and Audio',
                             'Social Impact'])
 
-    social_activity_ln = st.checkbox('LinkedIn')
-    social_activity_wb = st.checkbox('Website')
-    social_activity_tw = st.checkbox('Twitter')
-    social_activity_fb = st.checkbox('Facebook')
-    social_activity_ph = st.checkbox('Phone Number')
-    social_activity_em = st.checkbox('Contact Email')
     total_funding = st.number_input('Total Funding Amount (in USD)', min_value=0)
 
+    st.markdown('''
+                Please indicate the channels of social activity.
+                ''')
+    social_activity_wb = st.checkbox('Website')
+    social_activity_ph = st.checkbox('Phone Number')
+    social_activity_em = st.checkbox('Contact Email')
+    social_activity_ln = st.checkbox('LinkedIn')
+    social_activity_tw = st.checkbox('Twitter')
+    social_activity_fb = st.checkbox('Facebook')
+
     st.form_submit_button(label="Submit")
+
+
+
+params = {
+    "founded_date": founded_date,
+    "location": location,
+    "company_size": company_size,
+    "industry": industry,
+    "total_funding": total_funding,
+    "social_activity": [social_activity_wb,
+                        social_activity_ph,
+                        social_activity_em,
+                        social_activity_ln,
+                        social_activity_tw,
+                        social_activity_fb]
+}
+
+
+st.write(params)
