d9207b74cb870f3bc10a65348ab8621edda91963
Berkan Cetin <m.berkancetin@icloud.com>
2024-05-28 16:44:09 +0200

    Frontend development checkpoint-6

--- pages/benchmarks.py
+++ pages/benchmarks.py
@@ -1,7 +1,28 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 
 if st.button("Home"):
     st.switch_page("app.py")
 
-st.write('This is page 2')
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["Company Size", "Raised Funds", "Funding Rounds"])
+chart_data['Location'] = np.random.choice(['East','West','North', 'South'], 20)
+
+st.scatter_chart(
+    chart_data,
+    x='Company Size',
+    y='Raised Funds',
+    color='Location',
+    size='Funding Rounds',
+)
+
+
+
+chart_data = pd.DataFrame(np.random.randn(20, 8), columns=['10001+', '1001-5000',
+                                '101-250', '11-50',
+                                '251-500', '5001-10000',
+                            '501-1000', '51-100'])
+
+st.area_chart(chart_data)
