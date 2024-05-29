f6262c1aa5b17400a9da7c53d5bb12b6fd6e4685
Berkan Cetin <m.berkancetin@icloud.com>
2024-05-29 09:25:07 +0200

    Branch final check

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import streamlit as st
 import requests
 
+st.logo("startorb.jpg")
+
 st.markdown('''
-AI Powered Startup Success Prediction Tool.
+            ## StartOrb: Unlocking Tomorrow’s Success Today
+            Harness the Power of Data to Illuminate Your Startup Journey
 ''')
 
-# StartOrb: Unlocking Tomorrow’s Success Today
 # “Predicting Success with StartOrb”
-# Subheadline: “Harness the Power of Data to Illuminate Your Startup Journey”
-# st.image('images/orb.gif', caption='Orb gif image')
+
+st.image('images/orb.gif', caption='Orb gif image')
 
 col1, col2 = st.columns(2)
 
